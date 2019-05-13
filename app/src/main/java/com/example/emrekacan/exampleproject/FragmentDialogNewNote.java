cb2b89c6cac62d5dc406219c740aa768929f6458
emre.kacan <emre.kacan@amatisbilisim.com>
2019-05-13 10:36:29 +0300

    first register

---
+++ app/src/main/java/com/example/emrekacan/exampleproject/FragmentDialogNewNote.java
@@ -0,0 +1,20 @@
+package com.example.emrekacan.exampleproject;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentDialogNewNote extends DialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+        return inflater.inflate(R.layout.fragment_dialog,container,false);
+    }
+}
---
+++ app/src/main/java/com/example/emrekacan/exampleproject/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.emrekacan.exampleproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar mToolbar;
+    ImageView mBackgroundImageView;
+    Button mAddNewNote;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        setSupportActionBar(mToolbar);
+        mToolbar=findViewById(R.id.mainToolbar);
+        mBackgroundImageView=findViewById(R.id.imgBackground);
+        mAddNewNote=findViewById(R.id.addNoteButton);
+
+
+
+
+        Glide.with(this).load(R.drawable.bck).centerCrop().into(mBackgroundImageView);
+        mAddNewNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                noteDialogShow();
+            }
+        });
+
+    }
+
+    private void noteDialogShow() {
+
+        FragmentDialogNewNote newNote=new FragmentDialogNewNote();
+        newNote.show(getSupportFragmentManager(),"DialogNewNote");
+    }
+}
